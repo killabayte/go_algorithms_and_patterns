d0ffd33f6b2849e6bf1a065b8798425c912367fa
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-06 10:39:39 +0100

    Section one: start

--- dataStructures/main.go
+++ dataStructures/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	//Slices
+	//Slices - ordered
 	n := []int{1, 3, 5, 7, 9}
 	fmt.Println(n[len(n)-1])
 	fmt.Println(append(n, 11, 13, 15, 17, 19))
 
-	//Maps
+	//Maps - non ordered
 	sh := map[string]string{
 		"name":  "Invinceble",
 		"email": "invincible@amazon.com",
