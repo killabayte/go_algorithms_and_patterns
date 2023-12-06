3f0479627e06981bcaeb914261500a839949415e
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-06 10:45:37 +0100

    Section one: start

--- structsMethods/main.go
+++ structsMethods/main.go
@@ -1,10 +1,15 @@
 package main
 
-type User struct {
+type SuperHero struct {
 	Name, Role, Email string
 	Age               int
 }
 
 func main() {
+	sh := SuperHero{
+		Name: "Invincible",
+		Role: "Problem Solver",
+		Age:  18,
+	}
 
 }
