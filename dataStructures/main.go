3bd6f3a22a9a5437959e4a5f49cf037ae70ffc86
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-06 10:32:44 +0100

    Section one: start

--- dataStructures/main.go
+++ dataStructures/main.go
@@ -7,4 +7,11 @@ func main() {
 	n := []int{1, 3, 5, 7, 9}
 	fmt.Println(n[len(n)-1])
 	fmt.Println(append(n, 11, 13, 15, 17, 19))
+
+	sh := map[string]string{
+		"name":  "Invinceble",
+		"email": "invincible@amazon.com",
+		"role":  "problem solver",
+	}
+	fmt.Println(sh)
 }
