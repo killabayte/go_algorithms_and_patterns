4dff1470b3ba9b3fcfad4f31c7389f9c2e495e3b
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-06 10:53:41 +0100

    Section one: start

--- structsMethods/main.go
+++ structsMethods/main.go
@@ -7,6 +7,19 @@ type SuperHero struct {
 	Age               int
 }
 
+func (sh SuperHero) Salary() int {
+	switch sh.Role {
+	case "Junior problem solver":
+		return 500
+	case "Middle problem solver":
+		return 1000
+	case "problem solver":
+		return 50000
+	default:
+		return 0
+	}
+}
+
 func main() {
 	sh := SuperHero{
 		Name: "Invincible",
