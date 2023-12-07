0af1361c1a21e8278c7abc7e7da70d9587b29634
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-07 11:57:14 +0100

    Section one: start

---
+++ errorHandling/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type SuperHero struct {
+	Name, Role, Email string
+	Age               int
+}
+
+func (sh SuperHero) Salary() (int, error) {
+	switch sh.Role {
+	case "Junior problem solver":
+		return 500
+	case "Middle problem solver":
+		return 1000
+	case "problem solver":
+		return 50000
+	case "problem dealer":
+		return 500000
+	default:
+		return 0
+	}
+}
+
+func (sh *SuperHero) UpdateRole(role string) {
+	sh.Role = role
+}
+
+func main() {
+	sh := SuperHero{
+		Name: "Invincible",
+		Role: "problem solver",
+		Age:  18,
+	}
+
+	om := SuperHero{
+		Name: "Omni Man",
+		Role: "problem dealer",
+		Age:  3000,
+	}
+
+	fmt.Println(sh)
+	fmt.Printf("Super hero says: I'm %s\n", sh.Name)
+
+	fmt.Println(sh.Name, sh.Salary())
+	fmt.Println(om.Name, om.Salary())
+
+	sh.UpdateRole("Middle problem solver")
+	fmt.Println(sh.Name, sh.Salary())
+}
