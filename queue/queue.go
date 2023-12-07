df593cffb94e86aa741b2cc56708b8c59f170bae
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-07 18:32:31 +0100

    Section one: start

---
+++ queue/queue.go
@@ -0,0 +1,35 @@
+// package main
+
+// type Queue struct {
+// 	items []int
+// }
+
+// func (q *Queue) Enqueue(i int) {
+// 	q.items = append(q.items, i)
+// }
+
+// func (q *Queue) Dequeue() int {
+// 	if len(q.items) == 0 {
+// 		return -1
+// 	}
+// 	item, items := q.items[0], q.items[1:]
+// 	q.items = items
+// 	return item
+// }
+
+// func main() {
+// 	q := Queue{}
+// 	q.Enqueue(1)
+// 	q.Enqueue(2)
+// 	q.Enqueue(3)
+// 	q.Enqueue(4)
+// 	q.Enqueue(5)
+
+// 	println(q.Dequeue())
+// 	println(q.Dequeue())
+// 	println(q.Dequeue())
+// 	println(q.Dequeue())
+// 	println(q.Dequeue())
+// 	println(q.Dequeue())
+// 	println(q.Dequeue())
+// }
