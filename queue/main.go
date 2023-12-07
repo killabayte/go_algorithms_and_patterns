e22ef40f75dffe6da66f485808d7c92c6222da56
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-07 18:14:09 +0100

    Section one: start

--- queue/main.go
+++ queue/main.go
@@ -4,10 +4,10 @@ type Queue struct {
 	items []int
 }
 
-func (q *queue) Enqueue(i int) {
+func (q *Queue) Enqueue(i int) {
 }
 
-func (q *Dequeue) Dequeue() int {
+func (q *Queue) Dequeue() int {
 	return 0
 }
 
