5f607c3b2b85350e276e4aa09fd423fb528f86ae
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-07 12:36:26 +0100

    Section one: start

--- singleAndDoubleLinkedLists/main.go
+++ singleAndDoubleLinkedLists/main.go
@@ -12,7 +12,18 @@ func (l *List) Push(value int) {
 	//TBD
 }
 
+func (n *Node) Next() *Node {
+	return n.next
+}
+
 type Node struct {
 	value int
 	next  *Node
+}
+
+func main() {
+	l := &List{}
+	l.Push(1)
+	l.Push(2)
+	l.Push(3)
 }
