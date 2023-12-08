1c5e8a66c30d0b15d5304bd6e96967e3ed59aeed
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-08 10:42:00 +0100

    Section one: start

--- binarySearchAndTrees/btree.go
+++ binarySearchAndTrees/btree.go
@@ -4,8 +4,27 @@ type Tree struct {
 	node *Node
 }
 
+func (t *Tree) insert(value int) *Tree {
+	if t.node == nil {
+		t.node = &Node{value: value}
+	} else {
+		t.node.insert(value)
+	}
+	return t
+}
+
 type Node struct {
 	value int
 	left  *Node
 	right *Node
+}
+
+func (n *Node) insert(value int) {
+	if value <= n.value {
+		if n.left == nil {
+			n.left = &Node{value: value}
+		} else {
+			n.left.insert(value)
+		}
+	}
 }
