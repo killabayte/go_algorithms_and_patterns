4ef3a54ebf7161e5ec15af608817cd3b8c79c5ba
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-08 12:47:25 +0100

    Section one: start

--- workerPools/main.go
+++ workerPools/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
+func echoWorker(in, out chan int) {
+	for {
+		n := in
+		time.Sleep(
+			time.Duration(rand.Intn(3000)) * time.Millisecond,
+		)
+	}
+}
+
 func main() {
 	in := make(chan int)
 	out := make(chan int)
