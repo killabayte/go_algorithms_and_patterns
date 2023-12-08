d3b46e77342c9ad41bbb02558e23eed8a3c8be56
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-08 11:28:23 +0100

    Section one: start

--- generationChannels/main.go
+++ generationChannels/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
+func process2(ch chan int) {
+	n := rand.Intn(3000)
+	time.Sleep(time.Duration(n) * time.Millisecond)
+}
+
 func main() {
 	ch := make(chan int)
 	go process2()
