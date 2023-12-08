b7355bdeaf59a11de75f551105547b660c87f16d
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-08 11:55:24 +0100

    Section one: start

---
+++ fanInFanOut/fanin.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func sleep(){
+	time.Sleep(
+		time.Duration(rand.Intn(3000)) * time.Millisecond
+	)
+}
+
+func producer(ch chan<- int, name string) {
+	for {
+		sleep()
+		n := rand.Intn(100)
+		fmt.Printf("Channel %s -> %d\n", name, n)
+		ch <- n
+	}
+}
+
+func main() {
+
+}
