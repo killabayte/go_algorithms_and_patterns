0d3549430842e42aad32890f7a2b98374d12ecb9
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-16 10:26:16 +0100

    Section one: start

---
+++ awsPSDublicator/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
+)
+
+func main() {
+	var showHelp bool
+	flag.BoolVar(&showHelp, "h", false, "Show help message")
+	flag.Parse()
+
+	if showHelp {
+		printHelp()
+		os.Exit(0)
+	}
+
+	srcParameterName := os.Getenv("SRC_PARAMETER_NAME")
+	destParameterName := os.Getenv("DEST_PARAMETER_NAME")
+	region := os.Getenv("AWS_REGION")
+
+	if srcParameterName == "" || destParameterName == "" || region == "" {
+		fmt.Println("SRC_PARAMETER_NAME, DEST_PARAMETER_NAME, and AWS_REGION environment variables are required.")
+		printHelp()
+		os.Exit(1)
+	}
+
+	// Create an AWS session and SSM client
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	}))
+	ssmClient := ssm.New(sess)
+
+	// Check if the destination parameter exists
+	destParamInput := &ssm.GetParameterInput{
+		Name: aws.String(destParameterName),
+	}
+	destParamOutput, err := ssmClient.GetParameter(destParamInput)
+	if err != nil {
+		fmt.Printf("Error getting destination parameter %s: %v\n", destParameterName, err)
+		os.Exit(1)
+	}
+
+	// Get the source parameter value
+	srcParamInput := &ssm.GetParameterInput{
+		Name:           aws.String(srcParameterName),
+		WithDecryption: aws.Bool(true),
+	}
+	srcParamOutput, err := ssmClient.GetParameter(srcParamInput)
+	if err != nil {
+		fmt.Println("Error getting source parameter:", err)
+		os.Exit(1)
+	}
+
+	// Print the values before overwriting
+	fmt.Printf("Before Overwrite:\nSource Parameter (%s): %s\nDestination Parameter (%s): %s\n",
+		srcParameterName, *srcParamOutput.Parameter.Value,
+		destParameterName, *destParamOutput.Parameter.Value)
+
+	// Create or update the destination parameter
+	destParamInputPut := &ssm.PutParameterInput{
+		Name:      aws.String(destParameterName),
+		Value:     srcParamOutput.Parameter.Value,
+		Type:      srcParamOutput.Parameter.Type,
+		Overwrite: aws.Bool(true),
+	}
+	_, err = ssmClient.PutParameter(destParamInputPut)
+	if err != nil {
+		fmt.Println("Error copying parameter:", err)
+		os.Exit(1)
+	}
+
+	// Print the values after overwriting
+	fmt.Printf("\nAfter Overwrite:\nSource Parameter (%s): %s\nDestination Parameter (%s): %s\n",
+		srcParameterName, *srcParamOutput.Parameter.Value,
+		destParameterName, *srcParamOutput.Parameter.Value)
+
+	fmt.Printf("\nParameter %s successfully copied to %s\n", srcParameterName, destParameterName)
+}
+
+func printHelp() {
+	fmt.Println("Usage: AWS_REGION=${PUT-YOUR-REGION} SRC_PARAMETER_NAME=${PUT-YOUR-SOURCE-SSM-NAME} DEST_PARAMETER_NAME=${PUT-YOUR-DESTINATION-SSM-NAME} copy-ssm-parameters")
+	fmt.Println("\nOptions:")
+	flag.PrintDefaults()
+}
+
